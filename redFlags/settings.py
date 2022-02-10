8f21e759de1116338391088dacf80f485fe08a4a
artur <arturkabirov16@gmail.com>
2022-02-10 15:57:40 +0300

    Add basic auth

--- redFlags/settings.py
+++ redFlags/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
     'users'
 ]
 
@@ -69,6 +70,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['flags.filters.NoMarkupDjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
 }
 
 ROOT_URLCONF = 'redFlags.urls'
