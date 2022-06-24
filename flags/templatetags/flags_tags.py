c52dfcd4d740bdc5a3a6f1b9db80c9cdb6870d9c
Asan <dzhuzbaevasan@gmail.com>
2022-06-24 14:28:59 +0600

    Update flags_tags.py

--- flags/templatetags/flags_tags.py
+++ flags/templatetags/flags_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from flags.models import *
+from flags.flags_descriptions import flags_descriptions
 
 register = template.Library()
 
@@ -12,3 +13,13 @@ def get_all_entities():
 @register.simple_tag()
 def get_flag_types():
     return Irregularity.objects.all()
+
+
+@register.filter()
+def get_item(dictionary, key):
+    return dictionary.get(key)
+
+
+@register.simple_tag()
+def get_flag_description(irregularity_id):
+    return flags_descriptions.get(irregularity_id)
